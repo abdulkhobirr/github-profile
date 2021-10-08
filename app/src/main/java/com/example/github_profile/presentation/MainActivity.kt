2673755466107d7b83c8804167412ef985367279
abdulkhobir <abdulkhobir27@gmail.com>
2021-10-08 21:05:42 +0700

    Initial commit

---
+++ app/src/main/java/com/example/github_profile/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.github_profile.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.github_profile.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
